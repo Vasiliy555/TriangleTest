11f4d10041b3227b0e02c2b5d346f8fd0acd9bd8
Sergey <ddl2kir@yandex.ru>
2021-09-06 20:11:09 +0300

    some fix

--- src/test/java/geekbrains/TriangleTest.java
+++ src/test/java/geekbrains/TriangleTest.java
@@ -1,10 +1,36 @@
 package geekbrains;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+
 
 public class TriangleTest {
+    static final Logger logger = LoggerFactory.getLogger(TriangleTest.class);
+    public static int count;
+
+
+    @BeforeAll
+    static void setUpTest(){
+        logger.info("Start out tests");
+    }
+
+
+
+    @AfterEach
+    void tearDownTest(){
+        count++;
+
+        logger.info(count + " - test passed");
+
+
+    }
 
     @ParameterizedTest
     @CsvSource({
@@ -21,13 +47,14 @@ public class TriangleTest {
     @ParameterizedTest
     @CsvSource({
             "0, 4, 5",
-            "1, 4, 50",
+            "5, 4, 5",
             "3, 4, 5",
             "5, 5, 5",
             "29,41,14"
     })
      void determineTypeTest(int a, int b, int c) {
-        System.out.println(Triangle.determineType(a, b, c));
+        Triangle.determineType(a, b, c);
+        logger.info(Triangle.determineType(a, b, c));
 
     }
 
