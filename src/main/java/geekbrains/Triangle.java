188bfe7259740cedb7f1fa55acaebad7d64f8641
Sergey <ddl2kir@yandex.ru>
2021-09-07 07:55:29 +0300

    some fix

--- src/main/java/geekbrains/Triangle.java
+++ src/main/java/geekbrains/Triangle.java
@@ -8,15 +8,15 @@ public class Triangle {
     public static String determineType(int a, int b, int c) {
 
         if (a >= (b + c) || c >= (b + a) || b >= (a + c)) {
-            return "Not a Triangle";
+            return TriangleType.INVALID.getType();
         } else if (a == b && b == c) {
-            return "Equilateral Triangle";
+            return TriangleType.EQUILATERAL.getType();
         } else if (((a * a) + (b * b)) == (c * c) || ((a * a) + (c * c)) == (b * b) || ((c * c) + (b * b)) == (a * a)) {
-            return "Right Triangle";
+            return TriangleType.RIGHT.getType();
         } else if (a != b && b != c && c != a) {
-            return "Scalene Triangle";
+            return TriangleType.SCALENE.getType();
         } else if ((a == b && b != c) || (a != b && c == a) || (c == b && c != a)) {
-            return "Isosceles Triangle";
+            return TriangleType.ISOSCELES.getType();
         }
 
         return null;
